fa0fba80bb99ec392d941c63841252cb9cc0245b
Eduard <edikgoose@gmail.com>
2021-10-26 16:51:27 +0300

    Refactor UML

--- src/main/java/innopolis/university/differentialequationproject/MainPane.java
+++ src/main/java/innopolis/university/differentialequationproject/MainPane.java
@@ -1,17 +1,61 @@
 package innopolis.university.differentialequationproject;
 
+import innopolis.university.differentialequationproject.graphsControllers.GTEGraphsController;
+import innopolis.university.differentialequationproject.graphsControllers.GraphsController;
+import innopolis.university.differentialequationproject.graphsControllers.LTEGraphsController;
+import innopolis.university.differentialequationproject.graphsControllers.MainGraphsController;
+import innopolis.university.differentialequationproject.paneWrappers.SettingsPaneWrapper;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
 public class MainPane extends Application {
+    private GridPane gridPaneInitialize(){
+        GridPane gridPane = new GridPane();
+        ColumnConstraints columnForCharts = new ColumnConstraints();
+        columnForCharts.setPercentWidth(75);
+        ColumnConstraints columnForSetting = new ColumnConstraints();
+        columnForSetting.setPercentWidth(25);
+
+        RowConstraints mainRow = new RowConstraints();
+        mainRow.setPercentHeight(100);
+
+        gridPane.getColumnConstraints().addAll(columnForCharts, columnForSetting);
+        gridPane.getRowConstraints().add(mainRow);
+
+        return gridPane;
+    }
+
     @Override
     public void start(Stage primaryStage) {
-        MainGraphsPane mainGraphsPane = new MainGraphsPane();
-        Scene scene = new Scene(mainGraphsPane.getRoot());
+        GridPane root = gridPaneInitialize();
+        ObservableList<GraphsController> listOfLineCharts = FXCollections.observableArrayList();
+        listOfLineCharts.addAll(new MainGraphsController("Main Graphs"), new LTEGraphsController("Local Transaction Error"), new GTEGraphsController("Global Transaction Error"));
+        
+        TabPane tabPaneOfCharts = new TabPane();
+        for (var lineChart : listOfLineCharts) {
+            var tab = new Tab(lineChart.getChart().getTitle());
+            tab.setClosable(false);
+            tab.setContent(lineChart.getChart());
+            tabPaneOfCharts.getTabs().add(tab);
+        }
 
+        SettingsPaneWrapper settingsBox = new SettingsPaneWrapper(listOfLineCharts, tabPaneOfCharts);
+
+
+        root.add(settingsBox.getRoot(), 1, 0);
+        root.add(tabPaneOfCharts, 0, 0);
+
+
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-
         primaryStage.show();
     }
 
